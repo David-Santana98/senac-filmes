188b118f89edc8444a206ae24feef1bc2b8b00b5
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-05-10 17:15:48 -0300

    projeto senac-filmes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,22 @@
-
-
 export default function Home() {
+  const mario = {
+    titulo: "Super Mario ros.: O Filme",
+    poster:
+      "https://www.themoviedb.org/t/p/w220_and_h330_face/i9XdxHsFrcqLkRWSF1coOHo4R39.jpg",
+  };
+
+  const creed = {
+    titulo: "Creed III",
+    poster:
+      "https://www.themoviedb.org/t/p/w220_and_h330_face/wDugyjvDQsuwm9j9hkmK4whCOti.jpg",
+  };
   return (
-    
-  )
+    <>
+      <h1>Senac Filmes</h1>
+      <h2>Filmes em alta</h2>
+
+      <CardFilme filme={mario} />
+      <CardFilme filme={creed} />
+    </>
+  );
 }
