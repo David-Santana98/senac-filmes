ca1591d944d2847b3ac1b74c556ce0a60e93bdaf
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-05-10 18:18:20 -0300

    unsando metodo map

---
+++ src/components/SecaoFIlmes/index.jsx
@@ -0,0 +1,16 @@
+import CardFilme from "../CardFilme";
+
+export default function SecaoFilmes(props) {
+    const {titulo} = props
+    const {filmes} = props
+    return (
+        <>
+        <h2 className="text-2xl">{titulo}</h2>
+        <section>
+           {
+                filmes.map(filme => <CardFilme filme={filme} />)
+           }
+        </section>
+        </>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,26 @@
-import CardFilme from "@/components/CardFilme";
+import SecaoFilmes from "@/components/SecaoFIlmes";
 
 export default function Home() {
-  const mario = {
+
+  const filmes = [
+  {
     titulo: "Super Mario Bros.: O Filme",
     poster:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/i9XdxHsFrcqLkRWSF1coOHo4R39.jpg",
-  };
+  },
 
-  const creed = {
+  {
     titulo: "Creed III",
     poster:
       "https://www.themoviedb.org/t/p/w220_and_h330_face/wDugyjvDQsuwm9j9hkmK4whCOti.jpg",
-  };
+  }
+ ]
   return (
     <>
       <h1>Senac Filmes</h1>
 
-      <h2>Filmes em alta</h2>
-      <section>
-      <CardFilme filme={mario} />
-      <CardFilme filme={creed} />
-      </section>
-
-      <h2>Melhores dramas</h2>
-      <section>
-      <CardFilme filme={mario} />
-      <CardFilme filme={creed} />
-      </section>
+      <SecaoFilmes titulo="Filmes em alta" filme={filmes}/>
+      <SecaoFilmes titulo="Melhores dramas" filme={filmes}/>
     </>
   );
 }
