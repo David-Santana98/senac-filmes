003d3599b8caa239e2a11cb72c0867c7ff7ec190
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-05-10 17:28:58 -0300

    aplicando classes

--- src/components/CardFilme/index.jsx
+++ src/components/CardFilme/index.jsx
@@ -4,9 +4,9 @@ export default function CardFilme({filme}) {
     // const {filme} = props; destruturação
     return (
         <div>
-            <img src={filme.poster} alt="poster do filme" />
+            <img className="rounded-lg"  src={filme.poster} alt="poster do filme" />
             <h3 className="text-xl">{filme.titulo}</h3>
-            <a className="bg-amber-500"  href="#">detallhes</a>
+            <a className="bg-amber-500 text-black px-5 py-1 rounded"  href="#">detallhes</a>
         </div>
     )
 }
