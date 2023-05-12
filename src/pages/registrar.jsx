b781c67d4f0dfb361ebca9f0359e066403e37fae
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-05-12 11:19:27 -0300

    criando estado gerenciado

--- src/pages/registrar.jsx
+++ src/pages/registrar.jsx
@@ -3,53 +3,31 @@ export default function Registrar() {
   const [senha, setSenha] = useState("");
 
   return (
-    <div
-      className="flex flex-col items-center 
-        justify-center h-screen bg-slate-800"
-    >
-      <h1
-        className="text-4xl font-bold
-            text-amber-400"
-      >
-        Criar Conta
-      </h1>
-
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-800">
+      <h1 className="text-4xl font-bold text-amber-400">Criar Conta</h1>
       <form className="flex flex-col w-1/3">
-        <label
-          htmlFor="email"
-          className="text-amber-400
-                font-bold"
-        >
-          Email
-        </label>
+        <label htmlFor="email" className="text-amber-400 font-bold">Email</label>
         <input
+          className="rounded border-2 text-white border-slate-600 bg-slate-800 p-2 mb-4 focus:border-amber-400 focus:outline-none"
           id="email"
           type="email"
-          className="rounded border-2
-                text-white border-slate-600 bg-slate-800 p-2 mb-4
-                focus:border-amber-400 focus:outline-none"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
-        <label
-          htmlFor="senha"
-          className="text-amber-400
-                font-bold"
-        >
-          Senha
-        </label>
-        <input
+        <label htmlFor="senha" className="text-amber-400 font-bold">Senha</label>
+        <input 
+          className="rounded border-2 text-white border-slate-600 bg-slate-800 p-2 mb-4 focus:border-amber-400 focus:outline-none"
           id="senha"
           type="password"
-          className="rounded border-2
-                text-white border-slate-600 bg-slate-800 p-2 mb-4
-                focus:border-amber-400 focus:outline-none"
+          value={senha}
+          onChange={e => setSenha(e.target.value)}
+
+
+
         />
-        <button
-          className="rounded bg-amber-400 text-black px-5
-                py-2 hover:bg-amber-300"
-        >
-          Criar Conta
-        </button>
+        <button className="rounded bg-amber-400 text-black px-5 py-2 hover:bg-amber-300">
+          Criar Conta </button>
       </form>
     </div>
   );
